e27232d9ef4b4840f975d9f0d957974b6bddea22
Admin <contact@chipverify.com>
2017-01-17 09:31:30 -0600

    Update irun.f

--- file-includes/style3/irun.f
+++ file-includes/style3/irun.f
@@ -7,3 +7,12 @@
 ./tb/spi_agent.sv
 ./tb/my_pkg.sv
 ./tb/tb_top.sv
+
+/* Note that since we are providing all the files directly, 
+sometimes compilation order matters, and hence independent files
+are listed first, followed by dependents.
+
+For example, if tb_top.sv is listed before my_pkg.sv, then the 
+import construct in tb_top.sv will fail saying it can't find
+the package called my_pkg.sv
+*/
